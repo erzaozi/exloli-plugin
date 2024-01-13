516db58e4c513f28d0811a14e6a9fca0c84f5076
ZeroCal Su <3146312184@qq.com>
2024-01-13 15:20:48 +0800

    Update Setting.js

--- apps/Setting.js
+++ apps/Setting.js
@@ -21,6 +21,12 @@ export class Setting extends plugin {
                     reg: '^#?exloli(开启|关闭)推送$',
                     /** 执行方法 */
                     fnc: 'setting',
+                },
+                {
+                    /** 命令正则匹配 */
+                    reg: '^#?exloli设置token.*$',
+                    /** 执行方法 */
+                    fnc: 'token',
                 }
             ]
         })
@@ -28,7 +34,7 @@ export class Setting extends plugin {
 
     async setting(e) {
         let config = Config.getConfig();
-    
+
         const isGroupMessage = !!e.group_id;
         const list = isGroupMessage ? config.push_list.group : config.push_list.user;
         const id = isGroupMessage ? e.group_id : e.user_id;
@@ -41,15 +47,15 @@ export class Setting extends plugin {
                 return true;
             }
         }
-    
+
         if (isGroupMessage && e.sender.role !== 'owner' && e.sender.role !== 'admin') {
             await e.reply('只有管理员才可以开启推送哦~');
             return true;
         }
-    
+
         const index = list.indexOf(id);
         const isAlready = index !== -1;
-    
+
         if (isOpening) {
             if (isAlready) {
                 await e.reply('推送已经开启了哦~');
@@ -68,5 +74,21 @@ export class Setting extends plugin {
             }
         }
     }
-    
+
+    async token(e) {
+        if (!e.isMaster) {
+            e.reply('臭萝莉控滚开啊！变态！！');
+            return true;
+        }
+        const token = e.msg.replace(/^#?exloli设置token/, '').trim();
+        if (!token) {
+            e.reply("请提供一个有效的数据库Token，请加群 551081559 联系管理员（四條戀路）获取");
+            return true;
+        }
+        let config = Config.getConfig();
+        config.lolicon_token = token;
+        Config.setConfig(config);
+        e.reply("设置成功，请使用 #exloli克隆 命令克隆数据库，就可以使用啦~")
+        return true
+    }
 }
